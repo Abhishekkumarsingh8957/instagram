3c81bf844f3e7ef7e261b61b32e8a951a88162b4
Abhishekkumarsingh8957 <97937542+Abhishekkumarsingh8957@users.noreply.github.com>
2024-10-11 19:57:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://instagram-9beq.onrender.com");
+  res.setHeader("Access-Control-Allow-Origin", "https://ig-m7pu.onrender.com");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
